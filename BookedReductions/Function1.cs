a02de3981fda1ade0c9f5df00afe73e6757d9817
transtephan <96590733+transtephan@users.noreply.github.com>
2022-04-04 10:34:30 +0200

    Add project files.

---
+++ BookedReductions/Function1.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace BookedReductions
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public void Run([ServiceBusTrigger("QueueNameVMS", Connection = "ServiceBusConnectionVMS")]string myQueueItem, ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+        }
+    }
+}
