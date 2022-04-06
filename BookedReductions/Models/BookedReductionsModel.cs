381d1477912f67177e0bfb362008cbefb244c641
transtephan <96590733+transtephan@users.noreply.github.com>
2022-04-06 10:54:02 +0200

    data schema complete...ish

--- BookedReductions/Models/BookedReductionsModel.cs
+++ BookedReductions/Models/BookedReductionsModel.cs
@@ -13,96 +13,60 @@ namespace AzureIntegration_BookedReductions.Models
         [Required]
         public string version { get; set; }
         [Required]
-        public Vmsheader VMSHeader { get; set; }
+        public VMSHeader VMSHeader { get; set; }
         [Required]
         public Header2 Header2 { get; set; }
     }
-    public class Vmsheader
-    {
-        public string type { get; set; }
-        public string[] required { get; set; }
-        public Properties1 properties { get; set; }
-    }
 
-    public class Properties1
-    {
-        public Recordtype RecordType { get; set; }
-        public Integrationobjectname IntegrationObjectName { get; set; }
-        public Fileversion FileVersion { get; set; }
-        public Vmsinstancecode VMSInstanceCode { get; set; }
-        public Planningmarketcode PlanningMarketCode { get; set; }
-        public Maincompanycode MainCompanyCode { get; set; }
-        public Sequencenumber SequenceNumber { get; set; }
-        public Vmstransactionid VMSTransactionID { get; set; }
-        public Transactiondate TransactionDate { get; set; }
-        public Transactiontime TransactionTime { get; set; }
-    }
 
-    public class Recordtype
+    public class VMSHeader
     {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Integrationobjectname
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Fileversion
-    {
-        public string type { get; set; }
-    }
-
-    public class Vmsinstancecode
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Planningmarketcode
-    {
-        public string type { get; set; }
-    }
-
-    public class Maincompanycode
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Sequencenumber
-    {
-        public string type { get; set; }
-    }
-
-    public class Vmstransactionid
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Transactiondate
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Transactiontime
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
+        public VMSHeader(
+            string _recordType,
+            string _integrationObjectName,
+            int _fileVersion,
+            string _vmsInstanceCode,
+            int _planningMarketCode,
+            string _mainCompanyCode,
+            int _sequenceNumber,
+            string _vmsTransactionId,
+            string _transactionDate,
+            string _transactionTime)
+        {
+            RecordType = _recordType;
+            IntegrationObjectName = _integrationObjectName;
+            FileVersion = _fileVersion;
+            VMSInstanceCode = _vmsInstanceCode;
+            PlanningMarketCode = _planningMarketCode;
+            MainCompanyCode = _mainCompanyCode;
+            SequenceNumber = _sequenceNumber;
+            VMSTransactionId = _vmsTransactionId;
+            TransactionDate = _transactionDate;
+            TransactionTime = _transactionTime;
+        }
+        [Required]
+        public string RecordType { get; set; }
+        [Required]
+        public string IntegrationObjectName { get; set; }
+        [Required]
+        public int FileVersion { get; set; }
+        [Required]
+        public string VMSInstanceCode { get; set; }
+        [Required]
+        public int PlanningMarketCode { get; set; }
+        [Required]
+        public string MainCompanyCode { get; set; }
+        [Required]
+        public int SequenceNumber { get; set; }
+        [Required]
+        public string VMSTransactionId { get; set; }
+        [Required]
+        public string TransactionDate { get; set; }
+        [Required]
+        public string TransactionTime { get; set; }
     }
 
     public class Header2
-    {
-        public string type { get; set; }
-        public string[] required { get; set; }
-        public Properties2 properties { get; set; }
-    }
-
-    public class Properties2
     {
         public Reductionheader ReductionHeader { get; set; }
         public Details Details { get; set; }
@@ -110,231 +74,110 @@ namespace AzureIntegration_BookedReductions.Models
 
     public class Reductionheader
     {
-        public string type { get; set; }
-        public string[] required { get; set; }
-        public Properties3 properties { get; set; }
-    }
-
-    public class Properties3
-    {
-        public Recordtype1 RecordType { get; set; }
-        public Borenumber BoreNumber { get; set; }
-        public Modulecode ModuleCode { get; set; }
-        public Location Location { get; set; }
-        public Fiscalcompany FiscalCompany { get; set; }
-        public Fiscalcurrency FiscalCurrency { get; set; }
-        public EOP EOP { get; set; }
-        public Bookedreductioncreationdate BookedReductionCreationDate { get; set; }
-        public Bookedreductionsentdate BookedReductionSentDate { get; set; }
-        public Orgmodulecode OrgModuleCode { get; set; }
-    }
-
-    public class Recordtype1
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Borenumber
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Modulecode
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Location
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Fiscalcompany
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Fiscalcurrency
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class EOP
-    {
-        public string type { get; set; }
-    }
-
-    public class Bookedreductioncreationdate
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Bookedreductionsentdate
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Orgmodulecode
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
+        public Reductionheader(
+            string _recordType,
+            string _boreNumber,
+            string _moduleCode,
+            string _location,
+            string _fiscalComapny,
+            string _fiscalCurrency,
+            bool _eop,
+            string _bookedReductionCreationDate,
+            string _bookedReductionSentDate,
+            string _orgModuleCode
+            )
+        {
+            RecordType = _recordType;
+            BoreNumber = _boreNumber;
+            ModuleCode = _moduleCode;
+            Location = _location;
+            FiscalCompany = _fiscalComapny;
+            FiscalCurrency = _fiscalCurrency;
+            EOP = _eop;
+            BookedReductionCreationDate = _bookedReductionCreationDate;
+            BookedReductionSentDate = _bookedReductionSentDate;
+            OrgModuleCode = _orgModuleCode;
+        }
+        public string RecordType { get; set; }
+        public string BoreNumber { get; set; }
+        public string ModuleCode { get; set; }
+        public string Location { get; set; }
+        public string FiscalCompany { get; set; }
+        public string FiscalCurrency { get; set; }
+        public bool EOP { get; set; }
+        public string BookedReductionCreationDate { get; set; }
+        public string BookedReductionSentDate { get; set; }
+        public string OrgModuleCode { get; set; }
     }
 
     public class Details
     {
-        public string type { get; set; }
-        public string[] required { get; set; }
-        public Properties4 properties { get; set; }
+        public Details(
+            string _recordType,
+            string _orderNumber,
+            string _season,
+            string _seasonYear,
+            string _department,
+            string _index,
+            string _colourId,
+            int _reducedPieces,
+            string _newPriceInFiscalCurrency,
+            string _oldPriceInFiscalCurrency,
+            string _totalPriceInFiscalCurrency,
+            string _notePosition,
+            string _ibmSizeCode,
+            string _sizeScaleCode,
+            string _sdsSizeCode,
+            int _corporateBrandId,
+            string _productId,
+            string _articleId,
+            string _priceTagProductNumber,
+            string _priceTagArticleNumber
+            )
+        {
+            RecordType = _recordType;
+            OrderNumber = _orderNumber;
+            Season = _season;
+            SeasonYear = _seasonYear;
+            Department = _department;
+            Index = _index;
+            ColourId = _colourId;
+            ReducedPieces = _reducedPieces;
+            NewPriceInFiscalCurrency = _newPriceInFiscalCurrency;
+            OldPriceInFiscalCurrency = _oldPriceInFiscalCurrency;
+            TotalPriceInFiscalCurrency = _totalPriceInFiscalCurrency;
+            NotePosition = _notePosition;
+            IBMSizeCode = _ibmSizeCode;
+            SizeScaleCode = _sizeScaleCode;
+            SDSSizeCode = _sdsSizeCode;
+            CorporateBrandId = _corporateBrandId;
+            ProductId = _productId;
+            ArticleId = _articleId;
+            PriceTagProductNumber = _priceTagProductNumber;
+            PriceTagArticleNumber = _priceTagArticleNumber;
+        }
+        public string RecordType { get; set; }
+        public string OrderNumber { get; set; }
+        public string Season { get; set; }
+        public string SeasonYear { get; set; }
+        public string Department { get; set; }
+        public string Index { get; set; }
+        public string ColourId { get; set; }
+        public int ReducedPieces { get; set; }
+        public string NewPriceInFiscalCurrency { get; set; }
+        public string OldPriceInFiscalCurrency { get; set; }
+        public string TotalPriceInFiscalCurrency { get; set; }
+        public string NotePosition { get; set; }
+        public string IBMSizeCode { get; set; }
+        public string SizeScaleCode { get; set; }
+        public string SDSSizeCode { get; set; }
+        public int CorporateBrandId { get; set; }
+        public string ProductId { get; set; }
+        public string ArticleId { get; set; }
+        public string PriceTagProductNumber { get; set; }
+        public string PriceTagArticleNumber { get; set; }
     }
-
-    public class Properties4
-    {
-        public Recordtype2 RecordType { get; set; }
-        public Ordernumber OrderNumber { get; set; }
-        public Season Season { get; set; }
-        public Seasonyear SeasonYear { get; set; }
-        public Department Department { get; set; }
-        public Index Index { get; set; }
-        public Colourid ColourId { get; set; }
-        public Reducedpieces ReducedPieces { get; set; }
-        public Newpriceinfiscalcurrency NewPriceInFiscalCurrency { get; set; }
-        public Oldpriceinfiscalcurrency OldPriceInFiscalCurrency { get; set; }
-        public Totalpriceinfiscalcurrency TotalPriceInFiscalCurrency { get; set; }
-        public Noteposition NotePosition { get; set; }
-        public Ibmsizecode IBMSizeCode { get; set; }
-        public Sizescalecode SizeScaleCode { get; set; }
-        public Sdssizecode SDSSizeCode { get; set; }
-        public Corporatebrandid CorporateBrandId { get; set; }
-        public Productid ProductId { get; set; }
-        public Articleid ArticleId { get; set; }
-        public Pricetagproductnumber PriceTagProductNumber { get; set; }
-        public Pricetagarticlenumber PriceTagArticleNumber { get; set; }
-    }
-
-    public class Recordtype2
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Ordernumber
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Season
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Seasonyear
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Department
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Index
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Colourid
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Reducedpieces
-    {
-        public string type { get; set; }
-    }
-
-    public class Newpriceinfiscalcurrency
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Oldpriceinfiscalcurrency
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Totalpriceinfiscalcurrency
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Noteposition
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Ibmsizecode
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Sizescalecode
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Sdssizecode
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Corporatebrandid
-    {
-        public string type { get; set; }
-    }
-
-    public class Productid
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Articleid
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Pricetagproductnumber
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
-    public class Pricetagarticlenumber
-    {
-        public string type { get; set; }
-        public string pattern { get; set; }
-    }
-
 }
+
+    
+
